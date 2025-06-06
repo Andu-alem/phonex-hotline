ca3fbe6236473f2ed6812067fedc48797e6319ed
Andualem <andualem.fereja12@gmail.com>
2025-06-06 17:50:41 +0300

    style: update global styling

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,16 @@ import { GameControlBox } from './components/game-control-box'
 function App() {
 
   return (
-    <div className="w-full sm:w-[70%] mx-auto min-h-[70vh] bg-gradient-to-l from-[#395099] via-[#194eae] to-[#395099] rounded-md pt-7 flex flex-col justify-between gap-3 border-2 border-[#c4872e]">
-      <div className="flex-grow flex flex-col justify-between sm:justify-end sm:gap-4">
-        <HistoryBox />
-        <TileBox />
-        <ModeToggler />
+    <main className="w-full min-h-[100vh] bg-zinc-800 flex justify-center items-center">
+      <div className="w-full sm:w-[70%] min-h-[70vh] bg-gradient-to-l from-[#395099] via-[#194eae] to-[#395099] rounded-md pt-7 flex flex-col justify-between gap-3 border-2 border-[#c4872e]">
+        <div className="flex-grow flex flex-col justify-between sm:justify-end sm:gap-4">
+          <HistoryBox />
+          <TileBox />
+          <ModeToggler />
+        </div>
+        <GameControlBox />
       </div>
-      <GameControlBox />
-    </div>
+    </main>
   )
 }
 
