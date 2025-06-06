454f363aea5dda381c317168ab8ca8cfa179c531
Andualem <andualem.fereja12@gmail.com>
2025-06-06 17:06:38 +0300

    style: style update

--- src/components/mode-toggler.tsx
+++ src/components/mode-toggler.tsx
@@ -6,8 +6,8 @@ export function ModeToggler() {
             <div className="flex bg-[#0f3d8e] rounded-full px-1 py-[2px]">
                 <img src={speedometer} alt="spedometer" className="w-3 h-3" />
                 <div className="space-x-1 mx-3 flex items-center">
-                    <input type="checkbox" />
-                    <label className="text-[10px]">High risk mode</label>
+                    <input id="risk-mode-toggler" type="checkbox" />
+                    <label htmlFor="risk-mode-toggler" className="text-[10px]">High risk mode</label>
                 </div>
             </div>
         </div>
