e1b44ef51b7c8507d499b54c068a1da9b675acb1
Andualem <andualem.fereja12@gmail.com>
2025-06-06 18:37:26 +0300

    style: update font

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { GameControlBox } from './components/game-control-box'
 function App() {
 
   return (
-    <main className="w-full min-h-[100vh] bg-zinc-800 flex justify-center items-center">
+    <main className="w-full min-h-[100vh] bg-zinc-800 flex justify-center items-center text-white">
       <div className="w-full sm:w-[70%] min-h-[70vh] bg-gradient-to-l from-[#395099] via-[#194eae] to-[#395099] rounded-md pt-7 flex flex-col justify-between gap-3 border-2 border-[#c4872e]">
         <div className="flex-grow flex flex-col justify-between sm:justify-end sm:gap-4">
           <HistoryBox />
