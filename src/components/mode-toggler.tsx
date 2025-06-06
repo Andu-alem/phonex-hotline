f4584106d7a1dc1514efd70736f4206c7de79bc8
Andualem <andualem.fereja12@gmail.com>
2025-06-06 17:41:18 +0300

    feat: add toggler

--- src/components/mode-toggler.tsx
+++ src/components/mode-toggler.tsx
@@ -5,10 +5,11 @@ export function ModeToggler() {
         <div className="flex justify-center">
             <div className="flex bg-[#0f3d8e] rounded-full px-1 py-[2px]">
                 <img src={speedometer} alt="spedometer" className="w-3 h-3" />
-                <div className="space-x-1 mx-3 flex items-center">
-                    <input id="risk-mode-toggler" type="checkbox" />
-                    <label htmlFor="risk-mode-toggler" className="text-[10px]">High risk mode</label>
-                </div>
+                <label className="flex justify-between items-center gap-1 mx-3 text-[10px]">
+                    <input type="checkbox" className="appearance-none peer" />
+                    <span className="w-6 h-3 flex items-center flex-shrink-0 p-1 bg-[#395099] rounded-full duration-300 ease-in-out peer-checked:bg-[#395099] after:w-[12px] after:h-[12px] after:bg-white after:rounded-full after:shadow-md after:duration-300 peer-checked:after:translate-x-2"></span>
+                    High risk mode
+                </label>
             </div>
         </div>
     )
